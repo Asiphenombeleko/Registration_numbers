b321a57852687b949defa75ba9694c6baacd9ace
Asiphenombeleko <asiphenombeleko33@gmail.com>
2023-05-22 19:37:15 +0200

    just the skeleton

--- js/regFactory.js
+++ js/regFactory.js
@@ -1,12 +1,12 @@
 function RegistrationNumbers(RegNo,Place){
 var allPLates = []
 
+
     function regNumbers(){
         if(Place.startsWith("CA") && RegNo === undefined){
             allPLates.push(Place)
             }
     }
-
     return {
         RegistrationNumbers,
         regNumbers
